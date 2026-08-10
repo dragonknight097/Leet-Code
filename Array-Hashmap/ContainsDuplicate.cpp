85c74ff9958c72e43e4d1278349d4d0174d6d53d
dragonknight097 <48743998+dragonknight097@users.noreply.github.com>
2026-08-10 18:19:19 +0700

    Add containsDuplicate function to check for duplicates
    Implemented a function to check for duplicates in a vector using an unordered set.

--- Array-Hashmap/ContainsDuplicate.cpp
+++ Array-Hashmap/ContainsDuplicate.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
 
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+		for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+			seen.insert(num);
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5, 1};
+    if (solution.containsDuplicate(nums)) {
+        cout << "Contains duplicate" << endl;
+    } else {
+        cout << "No duplicates" << endl;
+    }
+    return 0;
+}
