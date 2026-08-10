9f8db0e1493f3acc52badfa92b5643afa05c543c
dragonknight097 <48743998+dragonknight097@users.noreply.github.com>
2026-08-10 18:16:16 +0700

    Implement Two Sum algorithm in C++

--- Array-Hashmap/TwoSum.cpp
+++ Array-Hashmap/TwoSum.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> result = {};
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                result.push_back(mp[complement]);
+                result.push_back(i);
+                break;
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    cout << "Indices of the two numbers that add up to " << target << ": ";
+    for (int index : result) {
+        cout << index << " ";
+    }
+    cout << endl;
+    return 0;
+}
